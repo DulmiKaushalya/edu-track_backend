3145fb48ade770ec95c6b30820cd1b91a709eb58
DulmiKaushalya <dulmikaushalya02@gmail.com>
2025-08-11 15:30:36 +0530

    create a module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EduStudentModule } from './edu-student/edu-student.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dulmikaushalya02:3Va6PVRaygRRSHxO@cluster0.in13kj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+    EduStudentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
